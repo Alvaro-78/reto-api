57cc3da11815a57698442c869af3c119d0b290a9
Álvaro Aguerré Navarrete <alvaro78an@gmail.com>
2021-02-18 10:08:36 +0100

    Add updateById

--- js/storage.js
+++ js/storage.js
@@ -25,25 +25,14 @@ class Storage {
    return instanceElement ? instanceElement : new Error( "Element not found" );
   };
 
-  async updateById(id, object) {
+  async updateById(id, nextObject) {
 
-    try{
-      //Igualando element a object estamos diciendo que object tendrá el valor del objeto element
-      const element = await this.findById(id);
-      console.log(typeof element , typeof element !== 'error', element)
+    await this.findById( id );
 
-      return element;
+    this.state[ id - 1 ] = nextObject;
 
-    }catch{
-      
-      if( element !== 'error' ){
-        element = object;
-
-      };
-
-    };
-
-
+    return nextObject; 
+    
   };
 
 };
--- test/carControllerTest.js
+++ test/carControllerTest.js
@@ -2,6 +2,36 @@ const Storage = require('../js/storage')
 
 //Como indexAll es async para resolver la promesa hacemos el then()
 
+( async () => {
+
+  try {
+
+    let storage = new Storage();
+  
+    await storage.set({name: 'pepe', age: '23'})
+  
+    let resultado1 = await storage.set({name: 'pepe', age:'33'});
+  
+    let resultado2 = await storage.findById( 1 );
+  
+    console.log( resultado1, resultado2);
+  } 
+  catch ( error ) {
+    console.log( "Manejo de la excepción" )
+  }
+
+
+} ) ();
+
+
+
+
+
+
+
+
+
+
 let storage = new Storage();
 
 let resultado1 = storage.set({name: 'pepe', age:'33'});
